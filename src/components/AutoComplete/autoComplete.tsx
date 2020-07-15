c3f8dbe147f3f1dde826bae59dcb80a95a91e8d6
zhoujun180 <zhoujun180@SHMAC-C02WD4JJH.local>
2020-07-15 15:11:07 +0800

    autoComplete init

---
+++ src/components/AutoComplete/autoComplete.tsx
@@ -0,0 +1,190 @@
+import React, {
+  FC,
+  useState,
+  ChangeEvent,
+  KeyboardEvent,
+  ReactElement,
+  useEffect,
+  useRef,
+} from "react";
+import classNames from "classnames";
+import Input, { InputProps } from "../Input/input";
+import Icon from "../Icon/icon";
+import Transition from "../Transition/transition";
+import useDebounce from "../../hooks/useDebounce";
+import useClickOutside from "../../hooks/useClickOutside";
+
+interface DataSourceObject {
+  value: string;
+}
+
+export type DataSourceType<T = {}> = T & DataSourceObject;
+
+export interface AutoCompleteProps extends Omit<InputProps, "onSelect"> {
+  /**
+   * 返回输入建议的方法，可以拿到当前的输入，然后返回同步的数组或者是异步的Promise
+   * type DataSourceType<T = {}> = T & DataSourceObject
+   */
+  fetchSuggestions: (
+    str: string
+  ) => DataSourceObject[] | Promise<DataSourceType[]>;
+  /** 点击选中建议项时触发的回调 */
+  onSelect?: (item: string) => void;
+  /** 支持自定义渲染下拉项，返回ReactElement */
+  renderOption?: (item: DataSourceType) => ReactElement;
+}
+
+/**
+ * 输入框自动完成功能。当输入值需要自动完成时使用，支持同步和异步两种方式
+ * 支持 Input 组件的所有属性 支持键盘事件选择
+ * ### 引用方法
+ *
+ * ~~~js
+ * import { AutoComplete } from 'vikingship'
+ * ~~~
+ */
+export const AutoComplete: FC<AutoCompleteProps> = (props) => {
+  const {
+    fetchSuggestions,
+    onSelect,
+    value,
+    renderOption,
+    ...restProps
+  } = props;
+
+  //输入框value值
+  const [inputValue, setInputValue] = useState(value as string);
+  //下拉框的数据
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
+  //显示loading动画的state
+  const [loading, setLoading] = useState(false);
+  //是否显示下拉框
+  const [showDropdown, setShowDropdown] = useState(false);
+  //键盘操作时候的高亮 上下移动
+  const [highlightIndex, setHighlightIndex] = useState(-1);
+  //是否触发search
+  const triggerSearch = useRef(false);
+  const componentRef = useRef<HTMLDivElement>(null);
+  const debouncedValue = useDebounce(inputValue, 300);
+
+  useClickOutside(componentRef, () => {
+    setSuggestions([]);
+  });
+
+  useEffect(() => {
+    if (debouncedValue && triggerSearch.current) {
+      setSuggestions([]);
+      const results = fetchSuggestions(debouncedValue);
+      if (results instanceof Promise) {
+        setLoading(true);
+        results.then((data) => {
+          setLoading(false);
+          setSuggestions(data);
+          if (data.length > 0) setShowDropdown(true);
+        });
+      } else {
+        setSuggestions(results);
+        setShowDropdown(true);
+        if (results.length > 0) setShowDropdown(true);
+      }
+    } else {
+      setShowDropdown(false);
+    }
+    setHighlightIndex(-1);
+  }, [debouncedValue, fetchSuggestions]);
+
+  const highlight = (index: number) => {
+    if (index < 0) index = 0;
+    if (index >= suggestions.length) index = suggestions.length - 1;
+    setHighlightIndex(index);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch (e.keyCode) {
+      case 13:
+        if (suggestions[highlightIndex]) {
+          handleSelect(suggestions[highlightIndex]);
+        }
+        break;
+      case 38:
+        highlight(highlightIndex - 1);
+        break;
+      case 40:
+        highlight(highlightIndex + 1);
+        break;
+      case 27:
+        setShowDropdown(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setInputValue(value);
+    triggerSearch.current = true;
+  };
+
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value);
+    setShowDropdown(false);
+    if (onSelect) {
+      onSelect(item);
+    }
+    triggerSearch.current = false;
+  };
+
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value;
+  };
+
+  const generateDropdown = () => {
+    return (
+      <Transition
+        in={showDropdown || loading}
+        animation="zoom-in-top"
+        timeout={300}
+        onExited={() => {
+          setSuggestions([]);
+        }}
+      >
+        <ul className="godlike-suggestion-list">
+          {loading && (
+            <div className="suggstions-loading-icon">
+              <Icon icon="spinner" spin />
+            </div>
+          )}
+          {suggestions.map((item, index) => {
+            const cnames = classNames("suggestion-item", {
+              "is-active": index === highlightIndex,
+            });
+            return (
+              <li
+                key={index}
+                className={cnames}
+                onClick={() => handleSelect(item)}
+              >
+                {renderTemplate(item)}
+              </li>
+            );
+          })}
+        </ul>
+      </Transition>
+    );
+  };
+
+  return (
+    <div className="godlike-auto-complete" ref={componentRef}>
+      <Input
+        value={inputValue}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        {...restProps}
+      />
+      {generateDropdown()}
+    </div>
+  );
+};
+
+export default AutoComplete;
