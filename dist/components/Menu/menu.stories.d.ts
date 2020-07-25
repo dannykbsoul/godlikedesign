9096f4e161d62c07036368e3bac00ab8059abdfa
Dannysoul <346925954@qq.com>
2020-07-25 12:55:11 +0800

    dist

--- dist/components/Menu/menu.stories.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export declare const defaultMenu: () => JSX.Element;
-export declare const clickMenu: () => JSX.Element;
-export declare const openedMenu: () => JSX.Element;
